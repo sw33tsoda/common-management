09af376de134db51e4293d362daed48511d82a7d
sw33tsoda <sw33tsoda@gmail.com>
2024-07-12 15:53:00 +0700

    update(20240712): remove MUI and install sass

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,4 +1,3 @@
-import { Switch } from '@mui/material';
 import { useDispatch } from 'react-redux';
 import { GLOBAL_THEME_SYSTEM_TOGGLE } from './contexts/redux/reducers/themeSystem';
 import { LoginPage } from './pages';
@@ -6,16 +5,7 @@ import { LoginPage } from './pages';
 const App = () => {
     const dispatch = useDispatch();
 
-    return (
-        <>
-            <Switch
-                onChange={() => {
-                    dispatch(GLOBAL_THEME_SYSTEM_TOGGLE());
-                }}
-            />
-            <LoginPage />
-        </>
-    );
+    return <>hello</>;
 };
 
 export default App;
