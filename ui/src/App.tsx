6fa1c0eb741f7c30cc3af61b84e16824b4e1e622
sw33tsoda <sw33tsoda@gmail.com>
2024-08-11 22:50:23 +0700

    chore(20280811): cleanup app.tsx:

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -31,30 +31,6 @@ const App = () => {
         <div className='app'>
             <button onClick={getToken}>click to test token</button>
             <button onClick={test}>click to test api</button>
-            <Button variant={ButtonVariant.Outlined}>Happy</Button>
-            <Button variant={ButtonVariant.Plain}>New</Button>
-            <Button variant={ButtonVariant.Solid}>Year</Button>
-
-            <Button variant={ButtonVariant.Outlined} color={ButtonColor.Danger}>
-                Happy
-            </Button>
-            <Button variant={ButtonVariant.Plain} color={ButtonColor.Danger}>
-                New
-            </Button>
-            <Button variant={ButtonVariant.Solid} color={ButtonColor.Danger}>
-                Year
-            </Button>
-
-            <Button variant={ButtonVariant.Outlined} color={ButtonColor.Warning}>
-                Happy
-            </Button>
-            <Button variant={ButtonVariant.Plain} color={ButtonColor.Warning}>
-                New
-            </Button>
-            <Button variant={ButtonVariant.Solid} color={ButtonColor.Warning}>
-                Year
-            </Button>
-
             <button onClick={callApi}>call api</button>
             <Loading />
         </div>
